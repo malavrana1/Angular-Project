0004e893614332a804ef4142497e0c2559026d7d
malavrana1 <ranam211197@gmail.com>
2024-06-26 18:21:06 -0400

    Implemented Angular app with API integration, unit tests, and README documentation

---
+++ photo-app/src/app/services/photo-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../photo.service';
+import { Photo } from './photo.model';
+
+@Component({
+  selector: 'app-photo-list',
+  templateUrl: './photo-list.component.html',
+  styleUrls: ['./photo-list.component.css']
+})
+export class PhotoListComponent implements OnInit {
+  photos: Photo[] = [];
+
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit(): void {
+    this.photoService.getPhotos().subscribe(photos => {
+      this.photos = photos;
+    });
+  }
+}
